0daf8c92e3104d69a22e7997d6de838ace2649d6
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-02-09 18:02:15 +0530

    Remove redundant log

--- src/shield-util.js
+++ src/shield-util.js
@@ -1,7 +1,5 @@
 import { rule } from "graphql-shield";
 
 export const hasGrant = (jwt, grant) => {
-  let x = jwt["www.mujhub.com"].grants.includes(grant);
-  console.log(x);
-  return x;
+  return jwt["www.mujhub.com"].grants.includes(grant);
 }
